6a5b4eca651f25738ac657d18bde9e2e3acfdcc5
Al-amin07 <alsujon2001@gmail.com>
2024-06-12 00:18:19 +0600

    Payment Api added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const stripe = require('stripe')(process.env.STRIPE_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://fitness-b8a5d.web.app'],
   credentials: true
 }));
 app.use(express.json());
@@ -52,6 +52,7 @@ async function run() {
     const classCollection = client.db('fitness').collection('classs');
     const trainerCollection = client.db('fitness').collection('trainers');
     const appliedTrainerCollection = client.db('fitness').collection('appliedTrainers');
+    const communityCollection = client.db('fitness').collection('communitys');
     const paymentCollection = client.db('fitness').collection('payments');
     const subscriptionCollection = client.db('fitness').collection('subscriptions');
     const testimonialCollection = client.db('fitness').collection('testimonials');
@@ -81,6 +82,7 @@ async function run() {
       }
       next()
     }
+ 
 
     // User Role
 
@@ -140,12 +142,29 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users', async(req, res) => {
+      const users = req.body;
+      // console.log(data);
+      // console.log(data?.name, data?.email);
+      const query = {email: users?.email};
+      const updatedDoc = {
+        $set: {
+          name: users.name,
+          image: users.image
+        }
+      }
+
+      const result = await userCollection.updateOne(query, updatedDoc);
+      res.send(result)
+      
+    })
+
     // Get All Trainer
 
     app.get('/trainer', async (req, res) => {
 
       const result = await trainerCollection.find().toArray();
-      res.send(result)
+      res.send(result);
 
     })
 
@@ -156,10 +175,10 @@ async function run() {
       res.send(result)
     })
 
-    app.post('/trainers', async (req, res) => {
+    app.post('/trainers', verifyToken, verifyAdmin, async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
-      
+
       const updatedDoc = {
         $set: {
           role: 'trainer'
@@ -170,7 +189,7 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/trainer/:id', async (req, res) => {
+    app.delete('/trainer/:id', verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const emailQuery = { email: req.query.email };
 
@@ -187,28 +206,106 @@ async function run() {
 
     })
 
+    app.get('/trainers-email/:email', async (req, res) => {
+      const email = req.params.email;
+      // console.log('in trainer',email);
+      // res.send({message: 'Suces'})
+      const query = { email: email };
+      const result = await trainerCollection.findOne(query);
+      const appliedResult = await appliedTrainerCollection.findOne(query);
+    // // console.log('result',result);
+      res.send({result, appliedResult});
+     
+      
+    })
+
     app.get('/trainers/:email', async(req, res) => {
       const email = req.params.email;
-      // console.log(email);
-      // res.send({message: 'Suces'})
-      const query = { email: email};
-      const result = await trainerCollection.findOne(query);
-      res.send(result);
+      const result = await trainerCollection.findOne({email});
+      res.send(result)
+    })
+
+    app.put('/trainer/class',verifyToken, verifyTrainer, async (req, res) => {
+      const { newSlot, classDetails } = req.body;
+
+      const id = classDetails.trainerId
+
+      const trainer = await trainerCollection.findOne({ _id: new ObjectId(id) })
+
+      const { slots, time_in_day, classess } = trainer;
+      let arr = [];
+
+      if (slots) {
+        arr = [...slots, newSlot]
+      }
+      else {
+        arr = [newSlot]
+      }
+      console.log('inn arr ', arr);
+      // console.log(arr, time_in_day, typeof time_in_day);
+      const optiopns = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          slots: [...arr],
+          time_in_day: time_in_day - newSlot.slotTime
+        }
+      }
+
+      const result = await trainerCollection.updateOne({ _id: new ObjectId(id) }, updatedDoc, optiopns);
+
+      for (const classes of classDetails.selectedClassess) {
+        const query = { className: classes };
+        const mainClass = await classCollection.findOne(query);
+        const isExist = mainClass.teachers.find(item => item.id === classDetails.trainerId);
+       
+  
+       if(!isExist){
+        let array = [];
+        const { teachers } = mainClass;
+        const teacherCollec = {
+          value: classDetails.trainerFullName,
+          label: classDetails.trainerFullName,
+          image: classDetails.trainerImage,
+          id: classDetails.trainerId
+        }
+        console.log(teachers);
+        if (teachers.length > 0) {
+
+          array = [...teachers, teacherCollec]
+        }
+        else {
+          array = [teacherCollec]
+        }
+
+        const updatedDoc2 = {
+          $set: {
+            teachers: [...array]
+          }
+        }
+        const classResult = await classCollection.updateOne(query, updatedDoc2)
+        console.log(classResult);
+
+       }
+      }
+      res.send(result)
+
+
     })
 
 
-    app.put('/trainer/slot/:email', async(req, res) => {
+
+    app.put('/trainer/slot/:email', async (req, res) => {
       const email = req.params.email;
-      const slot = req.query.slot;
-      console.log('in Slot', email, slot);
-      const query = { email};
+      const index = parseInt(req.query.index);
+      // console.log('in Slot', email, typeof index);
+      const query = { email }
       const result = await trainerCollection.findOne(query);
-      const newSlot = result.available_slot.filter(item => item !== slot)
-      console.log(newSlot);
-      const optiopns = {upsert: true}
+      const newSlot = result.slots.filter((item, ind) => ind !== index)
+      // console.log(newSlot);
+      const optiopns = { upsert: true }
       const updatedDoc = {
         $set: {
-          available_slot: [...newSlot]
+          slots: [...newSlot]
         }
       }
 
@@ -218,21 +315,58 @@ async function run() {
 
     // Applied Trainer
 
+    app.get('/applied/:email', async(req, res) => {
+      const email = req.params.email;
+      console.log('in applied',email);
+      const query = { email}
+      const result = await appliedTrainerCollection.findOne(query);
+      if(result){
+        res.send(result)
+      }
+     else{
+    
+     }
+    })
+
     app.get('/applied-trainers', verifyToken, verifyAdmin, async (req, res) => {
       const query = { status: 'pending' }
       const result = await appliedTrainerCollection.find(query).toArray();
       res.send(result);
     })
 
-    app.post('/applied-trainers', async (req, res) => {
-      const user = req.body;
-      const result = await appliedTrainerCollection.insertOne(user);
+    app.get('/applied-all-trainer', async(req, res) => {
+      const result = await appliedTrainerCollection.find().toArray();
       res.send(result)
     })
 
- 
+    app.post('/applied-trainers', verifyToken, async (req, res) => {
+      const user = req.body;
+      // console.log('applied ', user);
+      const result = await appliedTrainerCollection.insertOne(user);
+      res.send(result)
+      // res.send({success: true})
+    })
 
-    app.delete('/applied-trainers/:id', async (req, res) => {
+
+    app.put('/admin-feedback', async(req, res) => {
+      const { item , feedback } = req.body;
+      // console.log(item, feedback);
+      const id = item._id
+      const query = { _id : new ObjectId(id)}
+      const options = {upsert: true};
+      const updatedDoc = {
+        $set: {
+          status : 'rejected',
+          feedback: feedback
+        }
+      }
+
+      const result = await appliedTrainerCollection.updateOne(query, updatedDoc, options);
+      res.send(result)
+    })
+
+
+    app.delete('/applied-trainers/:id', verifyToken, verifyAdmin,  async (req, res) => {
       const id = req.params.id;
       // console.log(id);
       const query = { _id: new ObjectId(id) };
@@ -245,6 +379,11 @@ async function run() {
     app.get('/classes', async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
+    })
+
+    app.get('/allclassess', async(req, res) => {
+      const result = await classCollection.find().sort({booked : -1}).toArray();
+      res.send(result)
     })
 
     app.post('/classes', verifyToken, async (req, res) => {
@@ -260,6 +399,30 @@ async function run() {
       res.send({ token })
     })
 
+    // Community
+
+    app.get('/community', async(req, res) => {
+      const result = await communityCollection.find().toArray();
+      res.send(result)
+    })
+    app.get('/community/time', async(req, res) => {
+      const result = await communityCollection.find().sort({time: -1}).toArray();
+      res.send(result)
+    })
+
+    app.get('/community-details/:id', async(req , res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await communityCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.post('/community', async(req, res) => {
+      const data = req.body;
+      const result = await communityCollection.insertOne(data);
+      res.send(result)
+    })
+
     // Subscription
 
     app.post('/subscription', async (req, res) => {
@@ -268,16 +431,73 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/subscription', verifyToken, verifyAdmin, async(req, res) => {
+    app.get('/subscription', verifyToken, verifyAdmin, async (req, res) => {
       const result = await subscriptionCollection.find().toArray();
       res.send(result)
     })
-    
+
     // Rating
 
-    app.get('/review', async(req, res) => {
+    app.get('/review', async (req, res) => {
       const result = await testimonialCollection.find().toArray();
       res.send(result)
+    })
+
+    // Admin DashBoard 
+
+    app.get('/admin-data',verifyToken, verifyAdmin, async(req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const classess = await classCollection.estimatedDocumentCount();
+      const payments = await paymentCollection.estimatedDocumentCount();
+      const uniqueUser = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: '$email'
+          }
+        },
+        {
+          $count: 'uniqueUserCount'
+        }
+      ]).toArray();
+
+      const uniqueSubs = await subscriptionCollection.aggregate([
+        {
+          $group: {
+            _id: '$email'
+          }
+        },
+        {
+          $count: 'uniqueSubsCount'
+        }
+      ]).toArray();
+
+      const uniqueSubsCount = uniqueSubs.length > 0 ? uniqueSubs[0].uniqueSubsCount : 0;
+
+      const uniqueUserCount = uniqueUser.length > 0 ? uniqueUser[0].uniqueUserCount : 0;
+      console.log('Users : ',uniqueUserCount);
+      console.log('Subs : ',uniqueSubsCount);
+
+      const paymentsItem = await paymentCollection.find().sort({time: -1}).toArray();
+      const result = await paymentCollection.aggregate([
+        {
+          $group: {
+            _id: null,
+            totalPrice: { $sum: '$price'}
+          }
+        }
+      ]).toArray();
+      const price=  result.length > 0 ? result[0].totalPrice : 0;
+      console.log(price);
+      
+      res.send({
+        users,
+        classess,
+        payments,
+        price, 
+        paymentsItem, 
+        uniqueUserCount,
+        uniqueSubsCount
+      })
     })
 
     // Payment
@@ -298,81 +518,50 @@ async function run() {
     })
 
     app.post('/payments', async (req, res) => {
-      const {payment,bookedDetails, trainer} = req.body;
-      const filter = {className: payment.className};
-      // console.log('in Pay tr', payment);
-      // console.log('in Pay tr', bookedDetails);
-      // console.log('in Pay tr', trainer);
-      
-   const singleClass = await classCollection.findOne(filter);
-  //  console.log('class',singleClass);
-      if(payment.className){
-        const query = { className: payment.className}
-        const optiopns = { upsert: true}
+      const { payment, bookedDetails, trainer } = req.body;
+
+  const paymentResult = await paymentCollection.insertOne(payment)
+
+
+      // Class
+      for (const className of payment.className) {
+        const filter = { className }
+        const singleClass = await classCollection.findOne(filter)
+        // console.log(singleClass);
         const updatedDoc = {
           $set: {
-            booked : singleClass?.booked + 1 
+            booked: singleClass?.booked + 1
           }
         }
+        const result = await classCollection.updateOne(filter, updatedDoc);
+        console.log(result);
 
-        const bookedResult = await classCollection.updateOne(query, updatedDoc, optiopns);
-        // console.log('in Payment', bookedResult);
       }
-
       const id = trainer._id;
-      const query = { _id : new ObjectId(id)}
-      const trainers = await trainerCollection.findOne(query)
-      let arr = [];
-      if(trainers?.bookingDetails.length > 0){
-        arr = [...trainers?.bookingDetails, bookedDetails]
-      }
-      else{
-        arr = [bookedDetails]
-      }
+      const singleTrainer = await trainerCollection.findOne({ _id: new ObjectId(id) })
+      // console.log(singleTrainer);
+      const optiopns = { upsert: true }
+      const slotIndex = parseInt(bookedDetails.slotIndex);
+     
 
-      const optiopns = { upsert : true}
-      const updatedDoc = {
-        $set: {
-          bookingDetails: [...arr]
-        }
-      }
 
-      const bookedResult = await trainerCollection.updateOne(query, updatedDoc, optiopns)
-      const result = await paymentCollection.insertOne(payment);
-      res.send({result, bookedResult})
+
+
+
+      
+      const updatePath = `slots.${slotIndex}.bookingDetails`;
+      const updateDocument = {
+        $set: { [updatePath]: bookedDetails }
+      };
+      // const result = await collection.updateOne(filter, update);
+
+      const result = await trainerCollection.updateOne({_id: new ObjectId(id)}, updateDocument);
+      res.send({paymentResult, result})
+      
+
     })
 
-    app.put('/payments', async(req, res) => {
-      const {trainer, bookedDetails} = req.body;
-      // console.log('details',trainer, bookedDetails);
-      const id = trainer._id;
-      const filters = { _id: new ObjectId(id)}
-      
-      const trainers = await trainerCollection.findOne(filters)
-      // console.log('in trainer',trainers);
-      let arr = [];
-      if(trainers?.bookingDetails){
-        // console.log('inSide');
-          arr = [...trainers?.bookedDetails, bookedDetails]
-      }
-      else{
-        arr = [bookedDetails]
-      }
-      // console.log('in arr',arr);
-      const optiopns = { upsert: true}
-      const updatedDoc = {
-        $set: {
-          bookingDetails: arr
-        }
-      }
-
-      const booking = await trainerCollection.updateOne(filters, updatedDoc, optiopns)
-      // console.log('in Payment ',booking);
-      res.send(booking)
-      
-    })
-    // res.send({message: 'success'})
-    // })
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
